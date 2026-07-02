edf9bd7c1bec92a8f88f184e97f7fc7d42114fd6
KMurase730 <zm25031@josai.ac.jp>
2026-07-02 17:29:45 +0900

    Initial commit

---
+++ myjs.js
@@ -0,0 +1,23 @@
+alert('Hello,Murase!');
+alert(1+1);
+alert(Math.sqrt(1+1));
+alert(Math.sqrt(2)+Math.sqrt(3))
+alert(1/0)
+alert(0/0)
+
+alert(2+3)
+alert('2'+'3')
+alert(Number('2')+Number('3'))
+alert('2'+3)
+alert(Number('2')+3)
+
+
+function calculateArea(width, height) {
+　var area = width * height; // 面積計算
+　return area;}
+
+var width = 5;
+var height = 3;
+var rectangleArea = calculateArea(width, height); // 長方形の面積の計算
+
+console.log("長方形の面積は " + rectangleArea + " です");
